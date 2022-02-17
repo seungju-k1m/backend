1852925d20813841e13ebbeca8d5c903b10563dc
seungju-mmc <ksj970213@hanyang.ac.kjr>
2022-02-17 18:52:32 +0900

    .

--- NNArch.py
+++ NNArch.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 from copy import deepcopy
-from utils import constructNet
+from .utils import constructNet
 
 
 class Node:
--- __init__.py
+++ __init__.py
@@ -1,1 +1,6 @@
-from utils import constructNet
+from . import utils
+from . import NN
+from . import NNArch
+from . import PER
+from . import sumtree
+from . import utils
